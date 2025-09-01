c669f6ac0d5396ff293003939df8312550ceafcb
anorton90 <alyssianorton@unomaha.edu>
2025-09-01 01:20:06 +0000

    lab one

--- MadLib.py
+++ MadLib.py
@@ -1,16 +1,34 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name:Alyssia Norton
+#Date:8-31-25
+#Assignment:lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+adjective1 = input ("Enter an adjective: ")
+adjective2 = input ("Enter an adjective: ")
+noun1 = input ("Enter a noun: ")
+noun2 = input ("Enter a noun: ")
+noun3 = input ("Enter a plural noun ")
+adverb1 = input ("Enter an adverb ")
+verb1 = input ("Enter a verb ")
+adjective3 = input ("Enter an adjective ")
+noun4 = input ("Enter a plural noun ")
+adjective4 = input ("Enter an adjective ")
+adjective5 = input ("Enter an adjective ")
+adjective6 = input ("Enter an adjective ")
+noun5 = input ("Enter a plural noun")
+alphabet1 = input ("Enter a letter of alphabet ") 
   #Print the story with the user supplied words.
-
-
-
+print (adjective1 + "teachers always give out" + adjective2 + "assignments")
+print ("But, as everyone knows, if you want to pass all your classes so you can go to a/an " + noun1 + "and become president of a big international" + noun2 + "an have millions of " + noun3 + "in the bank, you must do your homework and study" + adverb1)
+print ("If you just sit around and " + verb1 + "you won't get ahead in life")
+print ("You must learn to pay attention to every " + adjective3 + "thing your teacher says")
+print ("Do not interrupt or whisper to other " + noun4 + "during class")
+print ("Be sure to have a nice " + adjective4 + "notebook 'n which you can write down anything the teacher says that seems " + adjective5)
+print ("Then go home and memorize all of these " + adjective6 + "notes")
+print ("And when your teacher gives a surprise quiz, you will know all of the " + noun5 + "and will get a/an " + alphabet1 + "as a grade for the class")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
